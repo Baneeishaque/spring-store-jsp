72f3e804129d5c1152fc1ac79e1e9e2973404a6a
Ronitta <ronittamaryjokim369@yahoo.com>
2020-06-02 18:27:08 +0530

    JSP Index

---
+++ spring_jstore/src/main/java/ndk/banee/spring_jstore/SpringJStoreApplication.java
@@ -0,0 +1,13 @@
+package ndk.banee.spring_jstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJStoreApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringJStoreApplication.class, args);
+    }
+}
